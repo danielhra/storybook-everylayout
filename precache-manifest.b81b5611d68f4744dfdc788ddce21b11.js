b793b8b3a259f39802886ee2c7508e9b9fe7537e
danielhra <danielhra@hotmail.com>
2020-01-30 21:26:24 +0000

    Deploying to gh-pages from master 6dbb743c688f44f8e6e4616c7896623db72845f5

---
+++ precache-manifest.b81b5611d68f4744dfdc788ddce21b11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e84853407f848dcc17ab4bfb9e60118",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1d365d0d44a6ecffd57",
+    "url": "/static/css/main.bd28c0db.chunk.css"
+  },
+  {
+    "revision": "f56cb41f5b31d2c20049",
+    "url": "/static/js/2.e9817c0a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e9817c0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1d365d0d44a6ecffd57",
+    "url": "/static/js/main.603a73d1.chunk.js"
+  },
+  {
+    "revision": "88a9cfd16aee8eefffec",
+    "url": "/static/js/runtime-main.5baba475.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
