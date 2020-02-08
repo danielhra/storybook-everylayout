ae2236942388fd9fd285a73e364db6088d4499bb
danielhra <danielhra@hotmail.com>
2020-02-08 15:17:13 +0000

    Deploying to gh-pages from  - 763d11d90f6ea888386ea6828837ed1828fa22d1 🚀

---
+++ precache-manifest.6826ba115dc3407f5292f8f620b81a29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0740265a1bfe61c3260961685d5e385b",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.06141c6632c8ec55a44c.bundle.js"
+  },
+  {
+    "url": "runtime~main.06141c6632c8ec55a44c.bundle.js"
+  },
+  {
+    "revision": "7de66708487db40b750a",
+    "url": "static/css/main.85609660.chunk.css"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "url": "vendors~main.06141c6632c8ec55a44c.bundle.js"
+  },
+  {
+    "url": "vendors~main.06141c6632c8ec55a44c.bundle.js.LICENSE.txt"
+  }
+]);
